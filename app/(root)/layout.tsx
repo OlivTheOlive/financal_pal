2c2df8851c07247898f477e1a15a333c95422a67
Olivie Bergeron <111806530+OlivTheOlive@users.noreply.github.com>
2024-09-21 22:10:33 -0400

    add sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import SideBar from "@/components/SideBar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Olive", lastName: "Berg" };
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <SideBar user={loggedIn} />
       {children}
     </main>
   );
